fa355b2206570244d9c1704b7bbb4b56307c6d76
Christian Meier <m.kristian@web.de>
2013-08-27 13:36:34 +0200

    added =1 version contraints spec

--- spec/artifact_spec.rb
+++ spec/artifact_spec.rb
@@ -1,3 +1,4 @@
+require File.expand_path( 'spec_helper', File.dirname( __FILE__ ) )
 require 'maven/tools/artifact'
 
 describe Maven::Tools::Artifact do
@@ -8,5 +9,10 @@ describe Maven::Tools::Artifact do
     Maven::Tools::Artifact.new( "sdas.asd", "das", "jar", "123", ["fds:fre"] ).to_s.must_equal 'sdas.asd:das:jar:123:[fds:fre]'
     Maven::Tools::Artifact.new( "sdas.asd", "das", "jar", "bla", "123", ["fds:fre", "ferf:de"] ).to_s.must_equal 'sdas.asd:das:jar:bla:123:[fds:fre,ferf:de]'
     Maven::Tools::Artifact.new( "sdas.asd", "das", "jar", "blub", "123", ["fds:fre", "ferf:de"] ).to_s.must_equal 'sdas.asd:das:jar:blub:123:[fds:fre,ferf:de]'
+  end
+
+  it 'should convert ruby version contraints' do
+    Maven::Tools::Artifact.from( :gem, 'rubygems:asd', '=1' ).to_s.must_equal 'rubygems:asd:gem:[1,1.0.0.0.0.1)'
+
   end
 end
